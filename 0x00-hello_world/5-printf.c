e13f9e2e696f3f678b57c5dc999081a452f87ae4
ChalitaK24 <chalurga@gmail.com>
2024-06-20 19:52:35 -0700

    3 to 6

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *  main - this maim function prints text
+ *
+ *  Retun: 0 when successful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	Return(0)
+i}
