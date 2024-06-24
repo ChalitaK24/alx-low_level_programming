4f41a173f929511b3428b0baf6f3e330cf298495
ChalitaK24 <chalurga@gmail.com>
2024-06-24 15:29:51 -0700

    quotre crr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,8 +9,9 @@
 
 int main(void)
 {
-	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, message, 59);
+	char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, quote, 57);
 
 	return (1);
 }
