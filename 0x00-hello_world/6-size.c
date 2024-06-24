3b1c45afc33628d4ad390ba0e925f47243005235
ChalitaK24 <chalurga@gmail.com>
2024-06-24 14:48:28 -0700

    sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,18 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - prints the size of vairous types
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("Size of a char: %d byte(s)", 1);
+	printf("Size of an int: %d byte(s)", 4);
+	printf("Size of a long int: %d  byte(s)", 4);
+	printf("SIze of a long long int: %d byte(s)", 8);
+	printf("Size of a float: %d byte(s)", 4);
+
+	return (0);
+}
