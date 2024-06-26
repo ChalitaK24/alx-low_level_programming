ce5db4521de55422fb937893cea54f6002a953e2
ChalitaK24 <chalurga@gmail.com>
2024-06-26 08:55:41 -0700

    putchar print

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - prints putchar
+ * main - prints _putchar
  *
  * Return: 0 success
  */
@@ -10,7 +10,13 @@ int main(void)
 {
 	_putchar (95);
 	_putchar (112);
-       _putchar('\n');
+	_putchar (117);
+	_putchar (116);
+	_putchar (99);
+	_putchar (104);
+	_putchar (97);
+	_putchar (114);
+	_putchar('\n');
 	
 	return (0);
 }
