260a4e0369c31b84a4e3cd8272cc811e985db5ed
ChalitaK24 <chalurga@gmail.com>
2024-06-26 09:33:24 -0700

    alphabet lowecase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - prints all lowercase letters
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	int lowerc;
+
+	for (lowerc = 97; lowerc <= 122; lowerc++)
+	{
+		_putchar(lowerc);
+		_putchar('\n');
+	}
+}
