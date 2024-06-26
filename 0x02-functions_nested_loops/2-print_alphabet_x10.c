caeb24fc71f9f7d4ebe19d283871534f8c1a135f
ChalitaK24 <chalurga@gmail.com>
2024-06-26 12:55:38 -0700

    10 lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints lowercse letters 10 times
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int x; int lowercase;
+
+	for (x = 0; x < 10; ++x)
+	{
+		for (lowercase = 97; lowercase <= 122; lowercase++)
+		{
+			_putchar(lowercase);
+	
+		}
+	
+	_putchar('\n');
+	}
+}
