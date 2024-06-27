a1d2730b49d244775c3e3a84989d68254637968c
ChalitaK24 <chalurga@gmail.com>
2024-06-26 18:24:07 -0700

    bl

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - prints absolte value of an integer
+ * @int: variable
+ *
+ * Return: 0
+ */
+
+int _abs(int);
+{
+	int n;
+
+	if (n < 0)
+		return (n * -1);
+	else
+		return (n);
+	return (0);
+}
