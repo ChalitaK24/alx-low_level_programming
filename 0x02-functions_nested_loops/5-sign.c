6cbca7e01f2ce4c4bfa1a4220be9a9b5ebe4dbdc
ChalitaK24 <chalurga@gmail.com>
2024-06-26 18:11:33 -0700

    5sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -16,14 +16,14 @@ int print_sign(int n)
 
 		return (1);
 	}
-	else if ((n) == 0)
+	else if (n < 0)
 	{
-		_putchar (30);
-
-		return (0);
-	}
-	else
 		_putchar (45);
 
-	return (-1);
+		return (-1);
+	}
+	else
+		_putchar (30);
+
+	return (0);
 }
