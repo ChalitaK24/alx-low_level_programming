9a1e85b25357102c05795b19427cd0b9e10845a7
ChalitaK24 <chalurga@gmail.com>
2024-07-10 21:11:38 -0700

    5 sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,22 +4,62 @@
  * _sqrt_recursion - returns the natural squre root of a number
  * @n: input num
  *
- * Return:
+ * Return: sqrt n
  */
+
+int isprime(int number) 
+{
+	int i;
+    if (number < 2) 
+    {
+      	    return 0;
+    }
+
+    for (i = 2; i * i <= number; ++i) 
+    {
+        if (number % i == 0) 
+	{
+            return 0; 
+        }
+    }
+    return 1;
+}
 
 int _sqrt_recursion(int n)
 {
-	int x = 0;
-
-	if (n < 0 && x * x > n)
+	if (n < 0)
 	{
 		return (-1);
 	}
 
+	if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+
+	if (n > 2 && isprime(n))
+	{
+		return (-1);
+	}
+
+	return (_sqrt_recursionx(n, n / 2));
+
+}
+
+int _sqrt_recursionx(int n, int x)
+{
 	if (x * x == n)
 	{
 		return (x);
 	}
-	
-	return (_sqrt_recursion(x + 1));
+
+	if (x * x > n)
+	{
+		return (_sqrt_recursionx(n, (x + n / x) / 2));
+	}
+
+	else
+	{
+		return (_sqrt_recursionx(n, (x + n / x) / 2));
+	}
 }
