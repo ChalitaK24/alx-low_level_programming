73df3b5b365ddb0e0083302b45d7811fff371a21
ChalitaK24 <chalurga@gmail.com>
2024-07-22 02:03:23 -0700

    betty crr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -3,29 +3,30 @@
 
 /**
  * _strstr - locates a substring
+ * @haystack: main string
+ * @needle: substring to look for in *haystack
  *
- *
- * Return: pointer if success or NULL 
+ * Return: pointer if success or NULL
  */
 
 char *_strstr(char *haystack, char *needle)
 {
 	if (*needle == '\0')
 	{
-		return haystack;
+		return (haystack);
 	}
 
 	while (*haystack != '\0')
 	{
 		char *h = haystack;
 		char *n = needle;
-	
+
 		while (*h == *n && *n != '\0')
 		{
 			h++;
 			n++;
 		}
-		
+
 		if (*n == '\0')
 		{
 			return (haystack);
