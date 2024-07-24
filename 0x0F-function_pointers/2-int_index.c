17c743f6ab24996256d3139ec0dd990a500e086a
ChalitaK24 <chalurga@gmail.com>
2024-07-24 15:22:47 -0700

    search for integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,10 +12,6 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (array == NULL || cmp == NULL)
-	{
-		return (-1);
-	}
 	if (size <= 0)
 	{
 		return (-1);
@@ -23,7 +19,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	while (i < size)
 	{
-		if (cmp(array[i] != 0))
+		if (cmp(array[i]) != 0)
 		{
 			return (i);
 		}
