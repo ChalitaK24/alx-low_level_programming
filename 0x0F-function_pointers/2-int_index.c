e151f01a8a5bf7a8ff735e552ab1e277fb00c08a
ChalitaK24 <chalurga@gmail.com>
2024-07-24 15:18:49 -0700

    search for integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,33 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - search for an integer
+ *
+ *
+ * Return: 1 or -1
+ *
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (array == NULL || cmp == NULL)
+	{
+		return (-1);
+	}
+	if (size <= 0)
+	{
+		return (-1);
+	}
+
+	while (i < size)
+	{
+		if (cmp(array[i] != 0))
+		{
+			return (i);
+		}
+		i++;
+	}
+	return (-1);
+}
