de692e790634f9559f3c765f504e4be85cca9205
ChalitaK24 <chalurga@gmail.com>
2024-07-24 14:06:28 -0700

    print func crr

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,14 @@
+#include "function_pointer.h"
+
+/**
+ * array_iterator - excutes a func given as parameter
+ * on each element of an array
+ *
+ *
+ * Return: pointer
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+
+}
