70b0550b19b9922ed56d10401db171d5dcb1cbd8
ChalitaK24 <chalurga@gmail.com>
2024-07-24 15:30:42 -0700

    index crr

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,14 +2,16 @@
 
 /**
  * int_index - search for an integer
- *
+ * @array: pointer to array of integers
+ * @size: number of elements in array
+ * @cmp: pointer to a function
  *
  * Return: 1 or -1
- *
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
+
 	int i = 0;
 
 	if (size <= 0)
