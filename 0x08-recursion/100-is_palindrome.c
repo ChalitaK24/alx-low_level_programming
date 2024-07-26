c016a9e92b80b0e52ae234bbfeb62d524f270228
ChalitaK24 <chalurga@gmail.com>
2024-07-26 05:23:36 -0700

    crr recur palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -3,23 +3,43 @@
 
 
 /**
- * is_palindrome - checks if string is palindrome
+ * ch_palindrome - iterates throughstring
+ * @s: string to be checked
+ * @a: first element of sting
+ * @z: last element of a string
+ *
+ * Return: 1, function or 0
+ */
+
+int ch_palindrome(char *s, int a, int z)
+{
+	int a;
+	int z;
+
+	if (a > z)
+	{
+		return (1);
+	}
+	if (s[a] != s[z])
+	{
+		return (0);
+	}
+
+	return (ch_palindrome(s, a + 1, z - 1));
+}
+
+
+/**
+ * is_palindrome -  string is palindrome
  * @s: string to be checked
  *
- * Return: 1 success 0 not
+ * Return: 1 or 0
  */
+
 
 int is_palindrome(char *s)
 {
-	int a = 0;
-	int z = strlen(s) - 1;
+	int l = strlen(s);
 
-	for ( ; a < z; a++, z--)
-	{
-		if (s[a] != s[z])
-		{
-			return (0);
-		}
-	}
-	return (1);
+	return (ch_palindrome(s, 0, l - 1));
 }
