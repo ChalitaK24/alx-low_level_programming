b8ddbeb942df34bafb08d60268d5df535612b3d7
ChalitaK24 <chalurga@gmail.com>
2024-07-26 09:42:49 -0700

    wildcard crr

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -16,7 +16,7 @@ int wildcmp(char *s1, char *s2)
 		{
 			return (1);
 		}
-		return (*s1 && wildcmp(s1 + 1, s2)) || wildcmp(s1, s2 + 1);
+		return ((*s1 && wildcmp(s1 + 1, s2)) || wildcmp(s1, s2 + 1));
 	}
 
 	if (!*s1 && !*s2)
@@ -26,7 +26,7 @@ int wildcmp(char *s1, char *s2)
 
 	if (*s1 == *s2)
 	{
-		return (wildcmp(s1 + 1, s2 + 1);
+		return (wildcmp(s1 + 1, s2 + 1));
 	}
 	return (0);
 }
