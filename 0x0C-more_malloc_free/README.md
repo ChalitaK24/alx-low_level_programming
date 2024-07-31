7a17f977ef40d375cae438541a9c2188d5ffe293
ChalitaK24 <chalurga@gmail.com>
2024-07-31 02:37:17 -0700

    var comit

---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+more dyanmic memory allocation
--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -13,40 +13,46 @@ void print_all(const char * const format, ...)
 	va_list args;
 	unsigned int i = 0, prt = 0;
 	char *str;
+	const char *separ = "";
 
 	va_start(args, format);
-	for (i = 0; format && format[i]; i++)
+	while (format && format[i])
 	{
 		if (prt)
 		{
-			printf(", ");
+			separ = ", ";
 		}
-		switch (format[i])
+		while (format && format[i)
 		{
-			case 'c':
-				printf("%c", va_arg(args, int));
-				prt = 1;
-				break;
-			case 'i':
-				printf("%d", va_arg(args, int));
-				prt = 1;
-				break;
-			case 'f':
-				printf("%f", va_arg(args, double));
-				prt = 1;
-				break;
-			case 's':
-				str = va_arg(args, char *);
-				if (!str)
-				{
-					str = "(nil)";
-				}
-				printf("%s", str);
-				prt = 1;
-				break;
-			default:
-				prt = 0;
-				break;
+			switch (format[i])
+			{
+				case 'c':
+					printf("%c", separ, va_arg(args, int));
+					prt = 1;
+					break;
+				case 'i':
+					printf("%d", separ, va_arg(args, int));
+					prt = 1;
+					break;
+				case 'f':
+					printf("%f", separ, va_arg(args, double));
+					prt = 1;
+					break;
+				case 's':
+					str = va_arg(args, char *);
+					if (!str)
+					{
+						str = "(nil)";
+					}
+					printf("%s", separ, str);
+					prt = 1;
+					break;
+				default:
+					i++;
+					break;
+			}
+			i++;
+			break;
 		}
 	}
 	printf("\n");
