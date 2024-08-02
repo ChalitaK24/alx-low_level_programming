85ab4954507a831d8eb5b8f55bfac93054e40013
ChalitaK24 <chalurga@gmail.com>
2024-08-01 19:07:50 -0700

    crr

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,18 +12,18 @@ size_t print_list(const list_t *h)
 {
 	size_t clst = 0;
 
-	while(h != NULL)
+	while (h != NULL)
 	{
-		if (h -> str == NULL)
+		if (h->str == NULL)
 		{
 			printf("[0] (nil)\n");
 		}
 		else
 		{
-			printf("[%u] %s\n", h -> len, h -> str);
+			printf("[%u] %s\n", h->len, h->str);
 		}
 		clst++;
-		 h = h -> next;
+		 h = h->next;
 	}
 	return (clst);
 }
