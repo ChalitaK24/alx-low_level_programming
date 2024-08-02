69218dd07af581a32e7eb739fd265a2945565538
ChalitaK24 <chalurga@gmail.com>
2024-08-01 19:04:32 -0700

    linked lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - prints all elements of a list_t list
+ *
+ *
+ * Return: clst
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t clst = 0;
+
+	while(h != NULL)
+	{
+		if (h -> str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", h -> len, h -> str);
+		}
+		clst++;
+		 h = h -> next;
+	}
+	return (clst);
+}
+
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+A linked list is a linear data structure that includes a series of connected nodes.
