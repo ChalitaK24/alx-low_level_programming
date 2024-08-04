1966925f73d6dacf7b2c9bcfe25b49c33775a154
ChalitaK24 <chalurga@gmail.com>
2024-08-04 07:05:38 -0700

    betty crr

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,10 +2,10 @@
 
 /**
  * add_node - adds a new node at the beginning of a list_t linked list,
- * @head: ptr to address of head of list 
+ * @head: ptr to address of head of list
  * @str: strong to be duplicates
  *
- * Return: NULL or address 
+ * Return: NULL or address
  */
 
 list_t *add_node(list_t **head, const char *str)
@@ -29,7 +29,7 @@ list_t *add_node(list_t **head, const char *str)
 	}
 
 	node_n->str = strd;
-	node_n->len =strlen(strd);
+	node_n->len = strlen(strd);
 	node_n->next = *head;
 
 	*head = node_n;
