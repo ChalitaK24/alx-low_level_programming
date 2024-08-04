c7d0e0ddb01515da3528bfeb1e996aa500995358
ChalitaK24 <chalurga@gmail.com>
2024-08-04 07:29:30 -0700

    freee list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,4 +1,22 @@
 #include "lists.h"
 
+/**
+ * free_list- frees a list_t list
+ *
+ *
+ * Return:
+ */
 
+void free_list(list_t *head)
+{
+	list_t *tmp;
 
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp->str);
+		free(tmp);
+	}
+
+}
