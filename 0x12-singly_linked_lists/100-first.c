0fd78a6b9edecb857f1d4b8aa307aa935351dbda
ChalitaK24 <chalurga@gmail.com>
2024-08-04 13:52:33 -0700

    betty crr

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,10 +6,9 @@
  * Return: void
  */
 
-void __attribute__((constructor)) prtbfmain();
 
-void prtbfmain()
+void prtbfmain(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
-
