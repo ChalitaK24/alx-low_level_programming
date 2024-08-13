eb3e5e97caaa57c5ee8364c469fcb00f7600a681
ChalitaK24 <chalurga@gmail.com>
2024-08-12 18:32:40 -0700

    wrte

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: number
+ *
+ * Return: binary
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int n;
+if :wq
+
