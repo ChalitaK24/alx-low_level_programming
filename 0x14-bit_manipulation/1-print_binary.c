120d5290751fc29c4a0e254d11660fe8dede0168
ChalitaK24 <chalurga@gmail.com>
2024-08-30 12:19:05 -0700

    bit wise

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,6 +9,19 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int n;
-if :wq
 
+	
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+
+	if (n & 1)
+	{
+		_putchar('1');
+	}
+	else
+	{
+		_putchar('0');
+	}
+}
