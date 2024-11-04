5c008043f795c3851f9cb1fe8329c09a2899116a
ChalitaK24 <schalitakel@gmail.com>
2024-11-03 17:00:51 -0800

    task th

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,24 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table;
+	hash_table_t *n_table;
 	unsigned long int i;
 
+	n_table = malloc(sizeof(hash_table_t));
 
+	if (n_table == NULL)
+		return NULL;
+
+	n_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (n_table->array == NULL)
+	{
+		free(n_table);
+		return NULL;
+	}
+
+	for (i = 0; i < size; i++)
+		n_table->array[i] = NULL;
+
+	n_table->size = size;
+	return n_table;
+}
