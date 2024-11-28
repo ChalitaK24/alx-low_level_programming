3a320d32f017deba238aefafb5180676fbcb7c67
ChalitaK24 <schalitakel@gmail.com>
2024-11-27 18:37:23 -0800

    task 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    """
+
+
+    """
+    perimeter = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+
+
+    return perimeter
